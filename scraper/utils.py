fadd580bb6004b1d737da3750922567a77814292
Shawn Dabi <2001401@sit.singaporetech.edu.sg>
2025-04-08 23:48:12 +0800

    Tested actor screen time and sentiment analysis features

--- scraper/utils.py
+++ scraper/utils.py
@@ -8,7 +8,7 @@ def save_results(data, account_name=None):
 
     timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
     account_suffix = f"_{account_name.replace(' ', '_').lower()}" if account_name else ""
-    filename = f"outputs/tiktok{account_suffix}_{timestamp}.csv"
+    filename = f"outputs/scraper/tiktok{account_suffix}_{timestamp}.csv"
 
     with open(filename, mode='w', encoding='utf-8', newline='') as f:
         writer = csv.DictWriter(f, fieldnames=[
